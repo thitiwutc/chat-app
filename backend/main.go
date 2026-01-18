bacfbf7a5f986da97b21cae7998d10d335d9eadc
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-01-18 19:24:27 +0700

    feat: add health check endpoints

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(healthcheck.New())
+	log.Fatal(app.Listen(":3000"))
 }
