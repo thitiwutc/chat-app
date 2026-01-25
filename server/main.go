d3619a32c1424ca3a71ba75e097aa39dbbc6a30e
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-01-25 23:10:37 +0700

    chore: rename backend to server

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 )
@@ -28,6 +29,26 @@ func main() {
 	})
 
 	app.Use(healthcheck.New())
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", msg)
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	}))
 
 	log.Fatal(app.Listen(":3000"))
 }
